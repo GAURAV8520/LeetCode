1904c6f89bec68bea45e9db406b0a384274c7565
Gaurav Deshmukh <136880099+GAURAV8520@users.noreply.github.com>
2024-09-29 09:36:30 +0530

    Update PrintSubsequence.java

--- Recursion/PrintSubsequence.java
+++ Recursion/PrintSubsequence.java
@@ -27,3 +27,54 @@ public class Main
 		
 	}
 }
+
+
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// to print only one sequence 
+import java.util.*;
+public class Main
+{
+    public static boolean printSubsequenceOfSum3(int index,ArrayList<Integer>ls,int sum,int arr[]){
+        if(index==arr.length){
+            if(sum==3){
+                System.out.print("[ ");
+                for(int i=0;i<ls.size();i++){
+                    System.out.print(ls.get(i)+" ");
+                }
+                System.out.print("]");
+                System.out.println();
+                
+                return true; //condition satisfied
+            }
+            return false; //condition not satisfied
+            
+        }
+        
+        ls.add(arr[index]);
+        sum+=arr[index];
+        if(printSubsequenceOfSum3(index+1,ls,sum,arr)==true){
+            return true;
+        }
+        ls.remove(ls.size()-1);
+        sum-=arr[index];
+        if(printSubsequenceOfSum3(index+1,ls,sum,arr)==true){
+            return true;
+        }
+        
+        return false;
+        
+    }
+	public static void main(String[] args) {
+		int arr[]={1,2,1};
+		ArrayList<Integer>ls=new ArrayList<>();
+		printSubsequenceOfSum3(0,ls,0,arr);
+	}
+}
