f7095a1b7ba74723ae1db62cf391b5cbbb9584fd
Gaurav Deshmukh <136880099+GAURAV8520@users.noreply.github.com>
2024-11-14 16:03:36 +0530

    Update LongestsubstringwithoutRepeatingChar.java

--- SlidingWindow/LongestsubstringwithoutRepeatingChar.java
+++ SlidingWindow/LongestsubstringwithoutRepeatingChar.java
@@ -28,3 +28,32 @@ class Solution {
         return maxlen;
     }
 }
+
+// optimal solution 
+// tc :O(N)
+// sc:O(256)
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character,Integer>map=new HashMap<>();
+        int l=0;
+        int r=0;
+        int n=s.length();
+        int maxlen=0;
+
+        while(r<n){
+            if(map.containsKey(s.charAt(r))){
+                if(map.get(s.charAt(r))>=l){
+                    l=map.get(s.charAt(r))+1;
+                }
+            }
+            int len=r-l+1;
+            maxlen=Math.max(maxlen,len);
+            map.put(s.charAt(r),r);
+            r++;
+
+        }
+
+        return maxlen;
+    }
+}
