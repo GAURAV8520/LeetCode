90bd0b57a297fbdaae9eaf13385f2ec6700d22ca
Gaurav Deshmukh <136880099+GAURAV8520@users.noreply.github.com>
2024-11-23 10:32:51 +0530

    Update searchInRotatedSortedArray.java

--- Binary Search/searchInRotatedSortedArray.java
+++ Binary Search/searchInRotatedSortedArray.java
@@ -1,3 +1,4 @@
+//for without duplicate 
 class Solution {
     public int search(int[] nums, int target) {
         int low=0;
@@ -28,3 +29,42 @@ class Solution {
         return -1;
     }
 }
+
+
+//with duplicate value
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid]){
+               if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            else{
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+        }
+        return false;
+    }
+}
+
